fd46f0be70047d9e29d5c5295eadc6e029a83992
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-06 19:48:59 +0200

    fix: run.sh sticky

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bspc rule -a "*" --one-shot sticky=on state=floating rectangle=800x500+560+250
+bspc rule -a "*" --one-shot state=floating rectangle=800x500+560+250
 cargo build && ./target/debug/rusty-ants
