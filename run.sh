de211e0355b614c728073c206de7e28bc6843363
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-14 21:02:23 +0200

    fix(bspwm): typo in run rectangle size

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bspc rule -a "*" --one-shot state=floating rectangle=800x500+560+250
+bspc rule -a "*" --one-shot state=floating rectangle=600x600+669+240
 cargo build && ./target/debug/rusty-ants
